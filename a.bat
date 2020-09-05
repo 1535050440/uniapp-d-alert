06c7bd0b3508068f8114d95252c9abe0cfe62584
deng <1415551519@qq.com>
2020-09-05 15:39:53 +0800

    x

---
+++ a.bat
@@ -0,0 +1,20 @@
+@echo off
+title digitalrain
+color 0b
+setlocal ENABLEDELAYEDEXPANSION
+for /l %%i in (0) do (
+set "line="
+for /l %%j in (1,1,80) do (
+set /a Down%%j-=2
+set "x=!Down%%j!"
+if !x! LSS 0 (
+set /a Arrow%%j=!random!%%3
+set /a Down%%j=!random!%%15+10
+)
+set "x=!Arrow%%j!"
+if "!x!" == "2" (
+set "line=!line!!random:~-1! "
+) else (set "line=!line! ")
+)
+set /p=!line!<nul
+)
